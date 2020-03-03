20dbd82f5244e33301b10d22382fd72947237a09
Demian Katz <demian.katz@villanova.edu>
2020-03-03 08:47:03 -0500

    Use fixture instead of core config for unit test. (#1570)
    - Resolves VUFIND-1384.

--- module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/OpenUrlTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/OpenUrlTest.php
@@ -305,9 +305,7 @@ class OpenUrlTest extends ViewHelperTestCase
     protected function getOpenUrl($rules = null, $config = [], $mockContext = null)
     {
         if (null === $rules) {
-            $json = __DIR__
-                . '/../../../../../../../../../config/vufind/OpenUrlRules.json';
-            $rules = json_decode(file_get_contents($json), true);
+            $rules = $this->getFixture('defaults.json');
         }
         if (null === $mockContext) {
             $mockContext = $this->getMockContext();
