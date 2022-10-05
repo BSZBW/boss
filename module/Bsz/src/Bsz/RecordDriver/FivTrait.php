ff3512a1f3eb233a7ba156c3dce5fb3fc0c9b950
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2022-10-05 11:06:41 +0200

    Append a prefix to PPN searches and fixed line indentation.

--- module/Bsz/src/Bsz/RecordDriver/FivTrait.php
+++ module/Bsz/src/Bsz/RecordDriver/FivTrait.php
@@ -38,7 +38,6 @@ trait FivTrait {
         } elseif ($type === 'partial_aspects') {
             $ind2 = 1;
         }
-
         foreach ($this->getMarcRecord()->getFields('938') as $field) {
             $suba = $field->getSubField('a');
             $sub2 = $field->getSubfield(2);
