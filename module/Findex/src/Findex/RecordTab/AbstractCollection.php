e039a4dbdd8bf2db5bca42c5828d7158dae724cd
Sebastian Sahli <sebastian.sahli@bsz-bw.de>
2023-09-08 15:53:02 +0200

    common: Added BszCommon module as a place for common GVI and Findex code

--- module/Findex/src/Findex/RecordTab/AbstractCollection.php
+++ module/Findex/src/Findex/RecordTab/AbstractCollection.php
@@ -20,7 +20,6 @@
 
 namespace Findex\RecordTab;
 
-use VuFind\RecordTab\AbstractBase;
 use VuFindSearch\ParamBag;
 use VuFindSearch\Query\Query as SearchQuery;
 use VuFindSearch\Service as SearchService;
@@ -29,29 +28,8 @@ use VuFindSearch\Service as SearchService;
  * @package Findex\RecordTab
  * @author Sebastian Sahli
  */
-abstract class AbstractCollection extends AbstractBase
+abstract class AbstractCollection extends \BszCommon\RecordTab\AbstractCollection
 {
-
-    const LIMIT = 50;
-
-    /**
-     * Search service
-     *
-     * @var \VuFindSearch\Service
-     */
-    protected $searchService;
-
-    /**
-     *
-     * @var array
-     */
-    protected $results;
-
-    /**
-     * @var string
-     */
-    protected $searchClassId;
-
     /**
      * @var array
      */
@@ -65,83 +43,42 @@ abstract class AbstractCollection extends AbstractBase
      */
     public function __construct(SearchService $search, $isils)
     {
-        $this->searchService = $search;
+        parent::__construct($search);
         $this->isils = $isils;
     }
 
-    public function getResults()
+    protected function getQuery(): SearchQuery
     {
-        if ($this->results === null) {
-            $id = $this->driver->getUniqueID();
-            //hierarchy_parent_id stores the data from MARC fields 773w, 800w, 810w, 830w
-            $queryStr = 'hierarchy_parent_id:' . $id;
-            $query = new SearchQuery($queryStr);
-
-            // in local tab, we need to filter by isil
-            $filterOr = [];
-            if ($this->isFL() === false) {
-                foreach ($this->isils as $isil) {
-                    $filterOr[] = 'collection_details:ISIL_' . $isil;
-                }
-            }
-            $params = new ParamBag();
-            $params->add('fq', implode(' OR ', $filterOr));
-            $params->add('fq', '-id:' . $id);
-
-            $params->add('sort', 'hierarchy_sort_str desc');
-            $params->add('hl', 'false');
-            $params->add('echoParams', 'ALL');
-
-            $record = $this->getRecordDriver();
-            $records = $this->searchService->search(
-                $record->getSourceIdentifier(),
-                $query,
-                0,
-                static::LIMIT,
-                $params
-            )->getRecords();
-
-            $this->results = [];
-            foreach ($records as $r) {
-                if($this->display($r)) {
-                    $this->results[] = $r;
-                }
-            }
-        }
-        return $this->results;
+        $id = $this->driver->getUniqueID();
+        //hierarchy_parent_id stores the data from MARC fields 773w, 800w, 810w, 830w
+        $queryStr = 'hierarchy_parent_id:' . $id;
+        return new SearchQuery($queryStr);
     }
 
-    /**
-     * Indicates whether a given record should be displayed in this tab
-     * @param $record
-     * @return bool
-     */
-    abstract protected function display($record): bool;
-
-    private function isFL()
+    protected function getParams(): ParamBag
     {
-        $last = '';
-        $status = false;
-        if (isset($_SESSION['Search']['last'])) {
-            $last = urldecode($_SESSION['Search']['last']);
+        $id = $this->driver->getUniqueID();
+        // in local tab, we need to filter by isil
+        $filterOr = [];
+        if ($this->isFL() === false) {
+            foreach ($this->isils as $isil) {
+                $filterOr[] = 'collection_details:ISIL_' . $isil;
+            }
         }
-        if (strpos($last, 'consortium:FL') !== false
-            || strpos($last, 'consortium:"FL"') !== false
-            || strpos($last, 'consortium:ZDB') !== false
-            || strpos($last, 'consortium:"ZDB"') !== false
-        ) {
-            $status = true;
-        }
-        return $status;
+        $params = new ParamBag();
+        $params->add('fq', implode(' OR ', $filterOr));
+        $params->add('fq', '-id:' . $id);
+
+        $params->add('sort', 'hierarchy_sort_str desc');
+        $params->add('hl', 'false');
+        $params->add('echoParams', 'ALL');
+
+        return $params;
     }
 
-    /**
-     * @return bool
-     * @throws \Exception
-     */
-    public function isActive()
+    private function isFL(): bool
     {
-        return parent::isActive() && (count($this->getResults()) > 0);
+        return false;
     }
 
 }
