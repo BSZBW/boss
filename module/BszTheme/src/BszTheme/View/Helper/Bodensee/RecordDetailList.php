559accfee2af4af77f142460ab2bd741aa5e9d9f
Sebastian Sahli <sebastian.sahli@bsz-bw.de>
2024-04-09 16:44:57 +0200

    Refactoring of teh RecordDetails system

--- module/BszTheme/src/BszTheme/View/Helper/Bodensee/RecordDetailList.php
+++ module/BszTheme/src/BszTheme/View/Helper/Bodensee/RecordDetailList.php
@@ -14,78 +14,111 @@ class RecordDetailList extends AbstractHelper
         $this->config = $fullViewConfig;
     }
 
-//    public function __invoke()
-//    {
-//        $retVal = [];
-//        foreach ($this->config->Items ?? [] as $key => $value) {
-//
-//            $defaultMethod = 'get' . $key;
-//            $method = $this->config->Methods->get($key, $defaultMethod);
-//
-//            $defaultTemplate = 'details/' . strtolower($key) . '.phtml';
-//            $template = $this->config->Templates->get($key, $defaultTemplate);
-//
-//            $retVal[] = [
-//                'label' => $value,
-//                'method' => $method,
-//                'template' => $template,
-//                'context' => $this->getContext($key, [])
-//            ];
-//        }
-//        return $retVal;
-//    }
-
     public function __invoke(string $section = 'Items'): array
     {
-        $retVal = [];
-        $list = $this->config->get($section)->toArray();
-        foreach ($list ?? [] as $key => $value) {
+        $config = $this->config->get($section);
+        return $config == null ? [] : $this->readSection($config);
+    }
 
-            $defaultMethod = 'get' . $key;
-            $default = [
-                'label' => $value,
-                'methods' => [
-                    'calls' => [[
+    protected function readSection(Config $section): array
+    {
+        $ignore = array_merge(['disabled', 'type'], $this->getDisabled($section));
+
+        $list = [];
+        foreach ($section->toArray() ?? [] as $value => $key) {
+            if (in_array($value, $ignore)) {
+                continue;
+            }
+
+            $default = $this->getDefaultItem($value);
+            $secConfig = $this->config->get($value);
+            if ($secConfig == null) {
+                $list[$key] = [$default];
+                continue;
+            }
+
+            if(!array_key_exists($key, $list)) {
+                $list[$key] = [];
+            }
+            $type = $secConfig->get('type', 'item');
+            if($type == 'list') {
+                $list[$key] = array_merge($list[$key], $this->readSection($secConfig));
+            }elseif($type == 'item') {
+                $list[$key][] =  $this->readItem($secConfig, $default);
+            }
+        }
+
+        ksort($list);
+
+        $retVal = [];
+        foreach ($list as $items) {
+            foreach ($items as $item) {
+                $retVal[] = $item;
+            }
+        }
+        return $retVal;
+    }
+
+    protected function readItem(Config $section, array $default): array
+    {
+        $template = $section->get('template', $default['template']);
+
+        return [
+            'label' => $section->get('label', $default['label']),
+            'methods' => [
+                'calls' => $this->getMethods($section, $default['methods']['calls'][0]['name']),
+                'important' => $this->extractImportant($section)
+            ],
+            'class' => $this->getHtmlClass($section),
+            'requirements' => $this->getRequirements($section),
+            'template' => $template,
+            'context' => $this->getContext2($section),
+            'allowEmpty' => $this->getAllowEmpty($section)
+        ];
+    }
+
+    protected function getDefaultItem(string $key): array
+    {
+        $defaultMethod = 'get' . $key;
+        return [
+            'label' => $key,
+            'methods' => [
+                'calls' => [
+                    [
                         'name' => $defaultMethod,
                         'domain' => 'driver',
                         'args' => []
-                    ]],
-                    'important' => []
+                    ]
                 ],
-                'class' => '',
-                'allowEmpty' => false,
-                'requirements' => [],
-                'template' => 'details/' . strtolower($key) . '.phtml',
-                'context' => []
-            ];
+                'important' => []
+            ],
+            'class' => '',
+            'allowEmpty' => false,
+            'requirements' => [],
+            'template' => 'details/' . strtolower($key) . '.phtml',
+            'context' => []
+        ];
+    }
 
-            $section = $this->config->get($key);
-            if($section == null) {
-                $retVal[] = $default;
-                continue;
-            }
-            $template = $section->get('template', $default['template']);
-
-            $retVal[] = [
-                'label' => $value,
-                'methods' => [
-                    'calls' => $this->getMethods($section, $defaultMethod),
-                    'important' => $this->extractImportant($section)
-                ],
-                'class' => $this->getHtmlClass($section),
-                'requirements' => $this->getRequirements($section),
-                'template' => $template,
-                'context' => $this->getContext2($section),
-                'allowEmpty' => $this->getAllowEmpty($section)
-            ];
+    protected function getDisabled(Config $section): array
+    {
+        $disabled = $section->get('disabled', '');
+        if(!is_array($disabled)){
+            $disabled = [$disabled];
+        }
+        $retVal = [];
+        foreach ($disabled as $item) {
+            $retVal += array_filter(
+                array_map('trim', explode(',', $item))
+            );
         }
         return $retVal;
     }
 
     protected function getMethods(Config $section, string $defaultName): array
     {
-        $methodNames =  $section->get('method', $defaultName);
-        if(is_string($methodNames)) {
+        $methodNames = $section->get('method', $defaultName);
+        if (is_string($methodNames)) {
             $retVal = $this->extractMethodName($methodNames, 'driver');
             $retVal['args'] = $this->extractArguments($section, []);
             return [$retVal];
@@ -100,25 +133,26 @@ class RecordDetailList extends AbstractHelper
         return $retVal;
     }
 
-    protected function getRequirements(Config $section) {
+    protected function getRequirements(Config $section)
+    {
         $requirements = $section->get('requirement');
-        if($requirements == null) {
+        if ($requirements == null) {
             $requirements = [];
-        }elseif (is_string($requirements)) {
+        } elseif (is_string($requirements)) {
             $requirements = [$requirements];
         } else {
             $requirements = $requirements->toArray();
         }
 
         $retVal = [];
-        foreach($requirements as $req) {
+        foreach ($requirements as $req) {
             $parts = array_map('trim', explode('::', $req));
             if (count($parts) > 1) {
                 $retVal[] = [
                     'domain' => $parts[0],
                     'name' => $parts[1]
                 ];
-            }else {
+            } else {
                 $retVal[] = [
                     'domain' => 'driver',
                     'name' => $parts[0]
@@ -128,10 +162,12 @@ class RecordDetailList extends AbstractHelper
         return $retVal;
     }
 
-    protected function extractMethodName(string $method, string $defaultDomain): array
-    {
+    protected function extractMethodName(
+        string $method,
+        string $defaultDomain
+    ): array {
         $parts = array_map('trim', explode("::", $method));
-        if(count($parts) > 1) {
+        if (count($parts) > 1) {
             return [
                 'name' => $parts[1],
                 'domain' => $parts[0]
@@ -146,7 +182,7 @@ class RecordDetailList extends AbstractHelper
     protected function extractArguments(Config $section, array $default): array
     {
         $argString = $section->get('arguments');
-        if($argString == null) {
+        if ($argString == null) {
             return $default;
         }
 
@@ -156,7 +192,7 @@ class RecordDetailList extends AbstractHelper
     protected function extractImportant(Config $section): array
     {
         $important = $section->get('important');
-        if($important !== null) {
+        if ($important !== null) {
             return array_filter(
                 array_map('trim', explode(",", $important))
             );
@@ -172,46 +208,19 @@ class RecordDetailList extends AbstractHelper
             $parts = explode(",", $contextVal);
             foreach ($parts as $p) {
                 $pair = explode(":", $p);
-                if(count($pair) !== 2) {
+                if (count($pair) !== 2) {
                     continue;
                 }
                 $key = trim($pair[0]);
                 $value = $pair[1];
 
-                if(!empty($key)) {
+                if (!empty($key)) {
                     $retVal[$key] = $value;
                 }
             }
             return $retVal;
         }
         return $contextVal->toArray();
-    }
-
-    protected function getContext(string $key, array $default): array
-    {
-        $retVal = [];
-
-        $section = $this->config->get('Context');
-        if($section == null) {
-            return $default;
-        }
-
-        $valueString = $this->config->Context->get($key, '');
-        $parts = explode(",", $valueString);
-        foreach ($parts as $p) {
-            $pair = explode(":", $p);
-            if(count($pair) !== 2) {
-                continue;
-            }
-            $key = trim($pair[0]);
-            $value = trim($pair[1]);
-
-            if(!empty($key)) {
-                $retVal[$key] = $value;
-            }
-        }
-
-        return $retVal;
     }
 
     protected function getAllowEmpty(Config $section): bool
