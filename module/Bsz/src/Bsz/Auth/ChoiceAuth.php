4e9291698f76e1e85b19e17d9a0cf5237df966c8
stefanwinkler <stefan.winkler@bsz-bw.de>
2026-09-08 16:58:03 +0200

    merge develop -> master: ChoiceAuth.php in Bsz-module: case shibboleth missing in map2ClassNames($input). Existed on prod-boss.

--- module/Bsz/src/Bsz/Auth/ChoiceAuth.php
+++ module/Bsz/src/Bsz/Auth/ChoiceAuth.php
@@ -86,6 +86,8 @@ class ChoiceAuth extends \VuFind\Auth\ChoiceAuth
             break;
             case 'adisauth': $output = 'adis';
             break;
+            case 'shibboleth': $output = 'Shibboleth';
+            break;
         }
         return $output;
     }
